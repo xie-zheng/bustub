eb2c8cb5cd03d768cce3e828f7e27cf30284a3a4
xiezheng <xiezheng.dev@outlook.com>
2023-03-21 20:22:27 +0800

    [proj0] task#4

--- src/include/execution/expressions/string_expression.h
+++ src/include/execution/expressions/string_expression.h
@@ -45,8 +45,18 @@ class StringExpression : public AbstractExpression {
   }
 
   auto Compute(const std::string &val) const -> std::string {
-    // TODO(student): implement upper / lower.
-    return {};
+    auto s = val.substr();
+    if (expr_type_ == StringExpressionType::Lower) {
+      for (auto &c : s) {
+        c = std::tolower(c);
+      }
+    }
+    if (expr_type_ == StringExpressionType::Upper) {
+      for (auto &c : s) {
+        c = std::toupper(c);
+      }
+    }
+    return s;
   }
 
   auto Evaluate(const Tuple *tuple, const Schema &schema) const -> Value override {
