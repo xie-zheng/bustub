2c443949c12a4ff59719082aa21b77d1398a2145
xiezheng <xiezheng.dev@outlook.com>
2023-03-21 16:45:36 +0800

    [proj0] task#2

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -1,4 +1,6 @@
 #include "primer/trie_store.h"
+#include <optional>
+#include <utility>
 #include "common/exception.h"
 
 namespace bustub {
@@ -11,20 +13,60 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // (2) Lookup the value in the trie.
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  root_lock_.lock();
+  auto root = root_;
+  root_lock_.unlock();
+
+  auto value = root.Get<T>(key);
+  if (value == nullptr) {
+    // not found
+    return std::nullopt;
+  }
+
+  return std::make_optional(ValueGuard(std::move(root), *value));
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  write_lock_.lock();
+
+  // get the old root
+  root_lock_.lock();
+  auto root = root_;
+  root_lock_.unlock();
+
+  // do the operation
+  root = root.Put(key, std::move(value));
+
+  // swap in old root
+  root_lock_.lock();
+  root_ = std::move(root);
+  root_lock_.unlock();
+
+  write_lock_.unlock();
 }
 
 void TrieStore::Remove(std::string_view key) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  write_lock_.lock();
+
+  // get the old root
+  root_lock_.lock();
+  auto root = root_;
+  root_lock_.unlock();
+
+  // do the operation
+  root = root.Remove(key);
+
+  // swap in old root
+  root_lock_.lock();
+  root_ = std::move(root);
+  root_lock_.unlock();
+
+  write_lock_.unlock();
 }
 
 // Below are explicit instantiation of template functions.
